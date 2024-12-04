72f16e922e428ec0edd731000c40d1bd2473a9c9
JoshHeafy <i2225401@continental.edu.pe>
2024-12-04 00:11:23 -0500

    fix: Added Canpop false in welcome pages

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -14,55 +14,59 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: const Sidebar(),
-      appBar: AppBar(
-        iconTheme: const IconThemeData(color: Colors.white, size: 30),
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+        drawer: const Sidebar(),
+        appBar: AppBar(
+          iconTheme: const IconThemeData(color: Colors.white, size: 30),
+          backgroundColor: primaryAppColor,
+          elevation: 0,
+        ),
         backgroundColor: primaryAppColor,
-        elevation: 0,
-      ),
-      backgroundColor: primaryAppColor,
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Center(
-                child: Image.asset(
-                  'assets/images/carLogo.png',
-                  width: 300,
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: Image.asset(
+                    'assets/images/carLogo.png',
+                    width: 300,
+                  ),
                 ),
-              ),
-              Text(
-                'Hola, ${obtenerSaludo()} encantado de conocerte!',
-                style: const TextStyle(fontSize: 30, color: Colors.white),
-                textAlign: TextAlign.center,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 70),
-                child: ElevatedButton.icon(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: primaryAppColor,
-                    side: const BorderSide(
-                      width: 2,
+                Text(
+                  'Hola, ${obtenerSaludo()} encantado de conocerte!',
+                  style: const TextStyle(fontSize: 30, color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 70),
+                  child: ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: primaryAppColor,
+                      side: const BorderSide(
+                        width: 2,
+                        color: primaryColor,
+                      ),
+                    ),
+                    onPressed: () => Get.toNamed('/home/pasajeros-map'),
+                    icon: const Icon(
+                      Icons.place,
                       color: primaryColor,
                     ),
-                  ),
-                  onPressed: () => Get.toNamed('/home/pasajeros-map'),
-                  icon: const Icon(
-                    Icons.place,
-                    color: primaryColor,
-                  ),
-                  label: const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                    child: Text(
-                      'Ver pasajeros cercanos',
-                      style: TextStyle(color: primaryColor, fontSize: 18),
+                    label: const Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      child: Text(
+                        'Ver pasajeros cercanos',
+                        style: TextStyle(color: primaryColor, fontSize: 18),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
--- lib/ui/pages/welcome/welcome_page.dart
+++ lib/ui/pages/welcome/welcome_page.dart
@@ -7,39 +7,46 @@ class WelcomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: primaryAppColor,
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Image.asset(
-                'assets/images/SolicitarViaje.png',
-                width: 250,
-              ),
-              const Text(
-                'Solicitar Conductores',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30, color: Colors.white),
-              ),
-              const Text(
-                '¿Ya estas con un pasajero?\nSolicite al conductor más cercano con un solo click',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontWeight: FontWeight.w600, color: Colors.white),
-              ),
-              SizedBox(
-                height: 40,
-                width: 250,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: primaryColor,
-                  ),
-                  onPressed: () => Get.toNamed('/home'),
-                  child: const Text('Continuar'),
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+        backgroundColor: primaryAppColor,
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Image.asset(
+                  'assets/images/SolicitarViaje.png',
+                  width: 250,
                 ),
-              ),
-            ],
+                const Text(
+                  'Solicitar Conductores',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                      color: Colors.white),
+                ),
+                const Text(
+                  '¿Ya estas con un pasajero?\nSolicite al conductor más cercano con un solo click',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontWeight: FontWeight.w600, color: Colors.white),
+                ),
+                SizedBox(
+                  height: 40,
+                  width: 250,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: primaryColor,
+                    ),
+                    onPressed: () => Get.toNamed('/home'),
+                    child: const Text('Continuar'),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
